674f0a46fea90b4e1d08844ee9fdeaa618d2e231
MMMatth <mat.loup04@gmail.com>
2022-03-31 17:27:37 +0200

    i modify the IA and she work

--- py/game.py
+++ py/game.py
@@ -19,18 +19,16 @@ class gameclass:
         pygame.init()
         self.bot_or_human = bot_or_human
         self.screen = pygame.display.set_mode((1168,826))
+        self.background = pygame.image.load('../img/background.png').convert_alpha()
 
         pygame.display.set_caption("Jeu de Nim") 
         pygame.display.set_icon(pygame.image.load("../img/icon.png"))
         
-        self.background = pygame.image.load('../img/background.png').convert_alpha()
         
         self.nbr_sticks = 12
         self.augment_score = False
         self.finish = False 
-        
-        
-        
+    
         self.player1 = joueur.Joueur()
         self.player2 = joueur.Joueur()
         self.random_player() # we def wo start
@@ -41,43 +39,25 @@ class gameclass:
             self.graph = graphe_oriente.GraphOriente()
             # self.create_graph()
             self.graph.save()
-        else: self.button_r={"1" : pg.bouton("../img/b1.png",1061,260,147,147),"2" : pg.bouton("../img/b2.png",1061,445,147,147), "3" : pg.bouton("../img/b3.png",1061,630,147,147), }
+            
+        else: 
+            self.button_r={"1" : pg.bouton("../img/b1.png",1061,260,147,147),"2" : pg.bouton("../img/b2.png",1061,445,147,147), "3" : pg.bouton("../img/b3.png",1061,630,147,147), }
+            
             
         self.button_l={"1" : pg.bouton("../img/b1.png",107,260,147,147),"2" : pg.bouton("../img/b2.png",107,445,147,147),"3" : pg.bouton("../img/b3.png",107,630,147,147),}
 
-        
-        self.button_yes_no = {
-            "1" : pg.bouton("../img/oui.png",435,560,147,147),
-            "2" : pg.bouton("../img/non.png",735,560,147,147)
-        }
+        self.button_yes_no = { "1" : pg.bouton("../img/oui.png",435,560,147,147),"2" : pg.bouton("../img/non.png",735,560,147,147) }
         
         
         
-    def create_graph(self):
-        """create_graph is a function for create a graphe used for the AI"""
-        for i in range(1,13):
-            self.graph.ajouter_sommet(i)
-            if i>3 :
-                self.graph.ajouter_arc(i,i-1)
-                self.graph.ajouter_arc(i,i-2)
-                self.graph.ajouter_arc(i,i-3)
-        self.graph.ajouter_arc(3,2)
-        self.graph.ajouter_arc(3,1)
-        self.graph.ajouter_arc(2,1)
     
-    def random_player(self):
-        """random_player is a function for random the player who start the game"""
-        if random.randint(1,2) == 1 :
-            self.player1.joue = True
-        else : 
-            self.player2.joue = True
+    
+    # i blit all images
     
     def iblitall(self):
         """iblitall is a function for blit all the picture on the screen"""
-        
         self.screen.blit(self.background,(0,0))
         if self.finish == True: #Si fin de partie
-            
             
             pg.text(str(self.player1.score),130, 440,"center", color = "white", size = 100).iblit(self.screen)
             pg.text(str(self.player2.score),1035, 440,"center", color = "white", size = 100).iblit(self.screen)
@@ -110,6 +90,8 @@ class gameclass:
             
             if self.player2.joue == True:
                 pg.text("Joueur 2",590, 70,"center",size = 100, color= "white").iblit(self.screen)
+    
+    # some simple stuff
             
     def change_round(self):
         """change_round is a function for changing the round"""
@@ -119,18 +101,32 @@ class gameclass:
             self.player2.joue = True
         else:
             self.player1.joue = True
-            self.player2.joue = False    
-    
+            self.player2.joue = False   
+            
+    def random_player(self):
+        """random_player is a function for random the player who start the game"""
+        if random.randint(1,2) == 1 :
+            self.player1.joue = True
+        else : 
+            self.player2.joue = True 
+            
+    def create_graph(self):
+        """create_graph is a function for create a graphe used for the AI"""
+        for i in range(1,13):
+            self.graph.ajouter_sommet(i)
+            if i>3 :
+                self.graph.ajouter_arc(i,i-1)
+                self.graph.ajouter_arc(i,i-2)
+                self.graph.ajouter_arc(i,i-3)
+        self.graph.ajouter_arc(3,2)
+        self.graph.ajouter_arc(3,1)
+        self.graph.ajouter_arc(2,1)
     
 
-    def restart_game(self):
-        """restart_game is a function for restart the game"""
-        self.finish = False
-        self.random_player()
-        self.nbr_sticks = 12
-        self.player1.gagne ,self.player2.gagne = False, False
-        self.background = pygame.image.load('../img/background.png')
     
+    
+    # we manage the button
+     
     def button(self,event):
         """
         button is a function for manage the button of the game
@@ -160,7 +156,44 @@ class gameclass:
             self.click_song.play()
             menu.main()
             
+    
+    # we manage when pc play or when human play
+    
+    def computer_play(self):
+        """
+        cmpt_plays is a function 
+        """
+        if self.bot_or_human == "bot" and self.player2.joue == True and self.nbr_sticks >1 :
             
+                list_neibor = self.graph.liste_sommets_issus(self.nbr_sticks)
+                
+                if len(list_neibor) == 0 : # if the bot have no solutions for win
+                    print("bot haven't found solution")
+                    self.win(self.player1)
+                    return "p2 defeat"
+                    
+                self.graph_back = self.nbr_sticks
+                
+                self.nbr_sticks = random.choice(list_neibor)
+                 
+                self.graph_after = self.nbr_sticks
+                
+                self.change_round()
+    
+    def human_plays(self,nbr):
+        """
+        human_plays is a function for supr a sticks when human_plays
+
+        Args:
+            nbr (int): nbr of stick for supr
+        """
+        if self.nbr_sticks>=int(nbr)+1 :
+            self.nbr_sticks -= int(nbr)
+            self.change_round() 
+
+
+    #  we manage the end of the game
+    
     def win(self,player_win):
         """
         win is a victory function that allows you to manage the victory
@@ -177,38 +210,9 @@ class gameclass:
             if self.player1.gagne:
                     self.graph.supprimer_arc(self.graph_back,self.graph_after)
                     self.graph.save()
-            
-    def cmpt_plays(self):
-        """
-        cmpt_plays is a function 
-        """
-        if self.bot_or_human == "bot":
-            if self.player2.joue == True and self.nbr_sticks >1 :
-                    #à compléter Partie B question 3) le player2 ordi doit choisir son coups grâce au graphe G des coups
-                    #il doit choisir au hasard parmi la liste des arcs issus du sommet correspondant au nb d'allumettes
-                    list_neibor = self.graph.liste_sommets_issus(self.nbr_sticks)
-                    if len(list_neibor) == 0 :
-                        self.player1.gagne = True
-                        self.player2.gagne = False
-                        return False
-                        
-                    self.graph_back = self.nbr_sticks
-                    self.nbr_sticks = random.choice(list_neibor)
-                    self.graph_after = self.nbr_sticks
-                    self.player2.joue=False
-                    self.player1.joue = True
-    
-    def human_plays(self,nbr):
-        """
-        human_plays is a function for supr a sticks when human_plays
-
-        Args:
-            nbr (int): nbr of stick for supr
-        """
-        if self.nbr_sticks>=int(nbr)+1 :
-            self.nbr_sticks -= int(nbr)
-            self.change_round() 
-            
+        self.end_game(player_win)
+        
+        
     def end_game(self,player_win):
         """
         end_game is function when the game is end
@@ -220,6 +224,14 @@ class gameclass:
         if self.augment_score == True :
             player_win.score+=1
             self.augment_score = False #permet de n'augmenter le score qu'une seule fois
+    
+    def restart_game(self):
+        """restart_game is a function for restart the game"""
+        self.finish = False
+        self.random_player()
+        self.nbr_sticks = 12
+        self.player1.gagne ,self.player2.gagne = False, False
+        self.background = pygame.image.load('../img/background.png')
                                       
         
 def main(bot_or_human):
@@ -240,13 +252,11 @@ def main(bot_or_human):
             
             if game.nbr_sticks == 1 and game.player1.joue == True :    
                 game.win(game.player2)
-                game.end_game(game.player2)
                 
             if game.nbr_sticks == 1 and game.player2.joue == True :    
-                game.win(game.player1)
-                game.end_game(game.player1)
-                
-            game.cmpt_plays()
+                game.win(game.player1)    
+                            
+            game.computer_play()
             pygame.display.update()
                 
 
