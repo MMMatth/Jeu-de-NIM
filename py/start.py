46e514f5a10aa99e1b5fa8dec2179d261506a2f4
MMMatth <mat.loup04@gmail.com>
2022-04-07 21:31:46 +0200

    i had a background music

--- py/start.py
+++ py/start.py
@@ -1,4 +1,19 @@
 import menu
+import pygame
 
+class music:
+    def __init__(self):
+        self.song = "../song/music.wav"
+        self.song = pygame.mixer.music.load(self.song)
+        self.song = pygame.mixer.music.set_volume(0.5)
+        
+    def play(self):
+        self.music = pygame.mixer.music.play(-1)
+
+    def stop(self):
+        self.music = pygame.mixer.music.stop()
+        
 if __name__ == '__main__':
+    pygame.init()
+    music().play()
     menu.main()
